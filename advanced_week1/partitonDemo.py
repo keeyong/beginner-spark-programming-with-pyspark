88c08a439dde89b96f23396a8f3536b5a74d5abe
Keeyong (Max) <keeyonghan@hotmail.com>
2024-10-29 16:35:41 -0700

    Update partitonDemo.py

--- advanced_week1/partitonDemo.py
+++ advanced_week1/partitonDemo.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
         .enableHiveSupport() \
         .getOrCreate()
 
-    df = spark.read.csv("data/new_orders.csv", header=True, inferSchema=True)
+    df = spark.read.csv("data/orders_new.csv", header=True, inferSchema=True)
 
     df = df.withColumn("order_month", substring(df.order_date, 0, 7))
     df.show(5)
     df.write.mode("overwrite").partitionBy("order_month").saveAsTable("order")
 
     spark.conf.set("spark.sql.adaptive.coalescePartitions.parallelismFirst", False)
-    df.write.mode("overwrite").partitionBy("order_month").coalesce(2).saveAsTable("order_v2")
+    df.coalesce(2).write.mode("overwrite").partitionBy("order_month").saveAsTable("order_v2")
